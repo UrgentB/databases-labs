934c293a79866416f0d9034d4a497e92c56648d5
Alexandr <bananakuki@yandex.ru>
2026-05-10 18:45:43 +0300

    (idz4) add sql script for 2m row generetion

---
+++ idz4/sql/2m_rows_gen.sql
@@ -0,0 +1,23 @@
+DELETE FROM events_distributed;
+
+INSERT INTO events_distributed (
+                                event_date, 
+                                event_time, 
+                                user_id, 
+                                session_id, 
+                                event_type, 
+                                page_url, 
+                                duration_ms)
+SELECT
+    toDate('2024-01-01') + (rand() % 365) as event_date,
+    event_date + INTERVAL (rand() % 86400) SECOND as event_time,
+    (number % 100000) + 1 as user_id,
+    toString(number+rand()) as session_id,
+    CASE
+        WHEN rand()%3=0 THEN 'похороны'
+        WHEN rand()%3=1 THEN 'свадьба'
+        ELSE 'день рождения'
+    END as event_type,
+    concat('https://example.com/page/', toString(number % 1000 + 1)) as page_url,
+    rand() % 5000 as duration_ms
+FROM numbers(2_000_000);
